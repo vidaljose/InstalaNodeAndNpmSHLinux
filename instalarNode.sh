a4bf7f1ac6959787776c11ba640643be12d71df3
Jose Miranda <vidaljose2004@gmail.com>
2021-09-15 09:47:20 -0300

    Primer commit

---
+++ instalarNode.sh
@@ -0,0 +1,53 @@
+
+#!/bin/bash
+
+#Recuerde dar permisos al archivo "sudo chmod +x Documentos/instalarNode.sh"
+
+echo "Instalando node y npm"
+
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y autoremove
+
+echo ""
+echo ""
+echo "Se actualizo el sistema"
+echo ""
+echo ""
+
+sudo apt -y install npm 
+sudo npm install npm@latest -g
+
+echo ""
+echo ""
+echo "Se instalo npm"
+echo ""
+echo ""
+
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n 16.6.2
+
+echo ""
+echo ""
+echo "Se instalo node"
+echo ""
+echo ""
+
+sudo apt-get -y install rar
+
+echo ""
+echo ""
+echo "Se instalo rar"
+echo ""
+echo ""
+
+sudo apt-get -y install chromium-browser
+
+echo ""
+echo ""
+echo "Se instalo chromium-browser"
+echo ""
+echo ""
+
+exit
